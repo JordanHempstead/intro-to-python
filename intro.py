f987f49c50b3dd170df2d81951ee1956f89f7372
JordanHempstead <tariqhempst@gmail.com>
2018-02-22 12:49:04 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,28 @@
+print(29/2)
+print("Jordan " * 5)
+print("Ha " * 2 + "Ho " * 2)
+print("*\n**\n***\n****")
+name= input("What is your name:")
+print ("----------")
+print (".::" + name + "::.")
+print ("----------")
+print("Some text" == "some text")
+print(-4 < -6)
+print("part" in "party!!1")
+age = input("What is your age?: ")
+age = int(age)
+print ( (age > -1) and (age < 100))
+math = input("What is 4+9?: ")
+if math == 13:
+    print ("Well Done :)")
+elif math != 13:
+    print ("Wrong ):")
+#B.O.B. V.1
+print("Hello human. I'm B.O.B. What is your name?")
+input()
+print("That's a lovely name.")
+print("Is there anything you'd like to know?")
+input()
+print("I'm a computer, not a human. Did you like that information?");
+input()
+print("Goodbye.");
